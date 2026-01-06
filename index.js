7f1147edf5a068bf9a465d55f70ee27ba4096602
Willian <willfreitassantos@gmail.com>
2026-01-06 16:09:11 -0300

    desafio de logica

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
-let nome = "Will";
-let xp = 5300;
+let herois = [
+  { nomeHeroi: "Will", xp: 8500 },
+  { nomeHeroi: "Nicolau", xp: 950 },
+  { nomeHeroi: "Yure", xp: 12000 },
+  { nomeHeroi: "Juvenal", xp: 2500 }
+];
 
-if (xp <= 1000) {
+for (let i = 0; i < herois.length; i++) {
+  // Variáveis
+  let nome = herois[i].nomeHeroi;
+  let xp = herois[i].xp;
+  let nivel = "";
+
+  if (xp <= 1000) {
     nivel = "Ferro";
-} else if (xp <= 2000) {
+  } else if (xp >= 1001 && xp <= 2000) {
     nivel = "Bronze";
-} else if (xp <= 5000) {
+  } else if (xp >= 2001 && xp <= 5000) {
     nivel = "Prata";
-} else if (xp <= 7000) {
+  } else if (xp >= 5001 && xp <= 7000) {
     nivel = "Ouro";
-} else if (xp <= 8000) {
+  } else if (xp >= 7001 && xp <= 8000) {
     nivel = "Platina";
-} else if (xp <= 9000) {
+  } else if (xp >= 8001 && xp <= 9000) {
     nivel = "Ascendente";
-} else if (xp <= 10000) {
+  } else if (xp >= 9001 && xp <= 10000) {
     nivel = "Imortal";
-} else {
+  } else {
     nivel = "Radiante";
-}
+  }
 
-console.log("O Herói de nome " + nome + " está no nível de " + nivel);
+  // Saída
+  console.log("O Herói de nome " + nome + " está no nível de " + nivel);
+}
