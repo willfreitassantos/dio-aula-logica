e5844b0a48c0ff1a06e0937e52f6efb373794c17
Willian <willfreitassantos@gmail.com>
2026-01-06 15:59:45 -0300

    desafio de logica

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-let nome = "Artemis";
-let xp = 8500;
+let nome = "Will";
+let xp = 5300;
 
 if (xp <= 1000) {
-  nivel = "Ferro";
+    nivel = "Ferro";
 } else if (xp <= 2000) {
-  nivel = "Bronze";
+    nivel = "Bronze";
 } else if (xp <= 5000) {
-  nivel = "Prata";
+    nivel = "Prata";
 } else if (xp <= 7000) {
-  nivel = "Ouro";
+    nivel = "Ouro";
 } else if (xp <= 8000) {
-  nivel = "Platina";
+    nivel = "Platina";
 } else if (xp <= 9000) {
-  nivel = "Ascendente";
+    nivel = "Ascendente";
 } else if (xp <= 10000) {
-  nivel = "Imortal";
+    nivel = "Imortal";
 } else {
-  nivel = "Radiante";
+    nivel = "Radiante";
 }
 
 console.log("O Herói de nome " + nome + " está no nível de " + nivel);
